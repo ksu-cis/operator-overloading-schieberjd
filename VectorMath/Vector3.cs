64f596351fffd7a0d6665c02e426c04d6c56f7d9
schieberjd <schieberjd@F1092-41-2314>
2019-12-09 14:47:05 -0600

    In class 12/9

--- VectorMath/Vector3.cs
+++ VectorMath/Vector3.cs
@@ -17,12 +17,67 @@ namespace VectorMath
             Z = z;
         }
 
+        public override string ToString()
+        {
+            return $"<{X}, {Y}, {Z}>";
+        }
+
         // Vector Addition
+        public static Vector3 Add(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
 
-        // Vector Subtraction 
+        public void Add(Vector3 other)
+        {
+            this.X += other.X;
+            this.Y += other.Y;
+            this.Z += other.Z;
+        }
 
-        // Vector Scaling 
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        // Vector Subtraction
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
+
+        // Vector Scaling
+        public static Vector3 operator *(double s, Vector3 v)
+        {
+            return new Vector3(s * v.X, s * v.Y, s * v.Z);
+        }
+
+        public static Vector3 operator *(Vector3 v, double s)
+        {
+            return new Vector3(s * v.X, s * v.Y, s * v.Z);
+        }
 
         // Vector Comparison
+        public static bool operator ==(Vector3 a, Vector3 b)
+        {
+            return a.X == b.X && a.Y == b.Y && a.Z == b.Z;
+        }
+
+        public static bool operator !=(Vector3 a, Vector3 b)
+        {
+            return a.X != b.X || a.Y != b.Y || a.Z != b.Z;
+        }
+
+        public static readonly Vector3 Zero = new Vector3(0, 0, 0);
+
+        public static bool operator true(Vector3 v)
+        {
+            return Vector3.Zero != v;
+        }
+
+        public static bool operator false(Vector3 v)
+        {
+            return Vector3.Zero == v;
+        }
     }
 }
